193d287a376a7387fbd9fc18856bf5ca1da309f4
Asfi Sabrin Neha <asfisabrin@gmail.com>
2026-09-12 07:03:11 +0600

    Update DoctorFeeForm.cs
    Added comments explaining doctor fee setup logic

--- AgriCare/DoctorFeeForm.cs
+++ AgriCare/DoctorFeeForm.cs
@@ -1,4 +1,5 @@
-﻿// This form sets the online advice fee and farm visit fee for a newly approved doctorusing System;
+﻿// This form sets the online advice fee and farm visit fee for a newly approved doctor
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
